7c4283cc0f635689acb02ae711cbc79a52ea3a04
BURRA SHIVA KRISHNA REDDY <123821882+ShivaKrishnaReddyBurra@users.noreply.github.com>
2024-01-31 21:30:16 +0530

    added profile_card folder

---
+++ profile_card/profile.js
@@ -0,0 +1,15 @@
+function next(val)
+{
+    if(val==1)
+        window.location.replace("luffy.html");
+    else if(val==2)
+        window.location.replace("ichigo.html");
+    
+}
+function pre(val)
+{
+    if(val==1)
+        window.location.replace("profile.html");
+    if(val==2)
+        window.location.replace("luffy.html");
+}
