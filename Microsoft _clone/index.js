e54be17bcb4e0fe43973e54c9082c8a2fe3c4ca8
ShivaKrishnaReddyBurra <reddyburrashivakrishna@gmail.com>
2024-02-11 12:24:12 +0530

    Updated search functionality code, Day-3

--- Microsoft _clone/index.js
+++ Microsoft _clone/index.js
@@ -3,32 +3,35 @@ document.getElementById("ser").onclick = function(){
     var div =document.getElementById("dis1");
     if(c==0){
         var a=document.getElementById("ser");
+        var n=document.createElement("div");
         var inp=document.createElement("input");
         inp.setAttribute("type","text");
-        inp.setAttribute("placeholder","Search");
+        inp.setAttribute("placeholder","Search Microsoft.com");
         c=1;
-        inp.style.width="1030px";
-        inp.style.height="30px";
-        inp.style.borderRadius="5px";
-        inp.style.border="1px solid black";
-        inp.style.position="absolute";
-        inp.style.marginLeft="12%";
+        var button=document.createElement("input");
+        button.setAttribute("type","button");
+        button.setAttribute("value","Cancel");
 
-        a.value="cancle"
-        div.style.display=div.appendChild(inp);
-        a.style.top="0px";
-        a.style.right="0px";
-        a.style.height="30px";
-        a.style.width="100px";
-        a.style.borderRadius="5px";
-        a.style.border="1px solid black";
-    
+        inp.style.width="1150px";
+        inp.style.height="40px";
+        inp.style.border="2px solid #0067b8";
+        button.style.width="80px";
+        button.style.height="40px";
+        button.style.border="1px solid black";
+        button.style.backgroundColor="white";
+        n.appendChild(inp);
+        n.appendChild(button);
+        n.style.display="flex";
+        n.style.gap="2px";
+        n.style.position="absolute";
+        n.style.top="0px";
+        n.style.marginLeft="12%";
+        div.style.display=div.appendChild(n);
+        button.onclick=function(){
+            div.removeChild(div.lastChild);
+            c=0;
+            
+        }
     }
-    else{
-        var a=document.getElementById("ser");
-        div.removeChild(div.lastChild);
-        c=0;
-        a.value="Search";
-        a.border="none";
-    }
+   
 }
