cb4c3f314ef4a9d3fa6577b475bede74776f98f1
ShivaKrishnaReddyBurra <reddyburrashivakrishna@gmail.com>
2024-02-23 16:18:48 +0530

    Started portfolio completed navbar, Day-1

---
+++ portfolio/port.js
@@ -0,0 +1,8 @@
+function side(){
+    var a = document.getElementById("sbar");
+    a.style.display = "flex";
+}
+function sclose(){
+    var a = document.getElementById("sbar");
+    a.style.display = "none";
+}
